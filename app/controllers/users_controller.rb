345acbdbd792ec98bf1fbbe93606c896b11360ca
karthikeyankkn <connecttokkn@hotmail.com>
2018-03-01 10:14:30 +0530

    completed crud operation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,61 @@
 class UsersController < ApplicationController
-  def new
-  end
+  # def new
+	before_action :user_params, only: [:create, :update]
+  # end
+  	def index
+    	render json: @user = User.all
+ 	end
+ 	
+	 def show
+	  render json: @user = User.find(params[:id])
+	 end
+	 def new
+	  @user = User.new
+	 end
+
+	 def create
+	  @user = User.new(user_params)
+
+	  # if @user.save
+	  # 	render json: @user, status: :created_successfully
+	  # else
+	  # 	render json: @user.errors.full_messages
+	  # end
+	  if @user.save
+	  	 render json: @user, status: :created
+	  else
+	  	render json: @user.errors.full_messages
+	  end
+	 end
+
+	 def name
+	 	render json: User.column_names
+	 end
+
+	 def allusername
+	 	render json: User.all.collect(&:name)
+	 end
+
+	 def destroy
+	 	@user = User.find(params[:id])
+	 	if @user.destroy
+	 		render json: @user
+	 	else
+	 		 render json: @user.errors.full_messages
+	 	end
+	 end
+
+	 	def update
+		 	@user = User.find(params[:id])
+		 	if @user.update(user_params)
+	      render json: @user
+	    else
+	      render json: @user.errors, status: :unprocessable_entity
+	    end
+	 	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :subname, :password)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  get  '/showall',to: 'users#index'
+  get  '/name', to: 'users#name'
+  get  '/allusername', to: "users#allusername"
+  resources :users
 end
